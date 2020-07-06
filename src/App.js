505cc0c43cae5af1bc3e9d23647413288e4376fe
faisalrahman36@hotmail.com <faisalrahman36@hotmail.com>
2020-07-06 19:38:18 +0500

    react fetch data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect,useState} from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let data = {title: "Waiting for data"};
+  //let data= null;  //use let not const as we will update data
+  const [todo,settodo] = useState(data);
+  const [isData,setData]= useState(false);
+  const [isFetching,setFetching]= useState(false);
+  useEffect( () => {
+    
+  
+    async function fetchData () {
+
+      setFetching(true);
+      const response1 = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+      console.log("response= ",response1);
+      
+      console.log("Data = ",todo);
+      let data2=await response1.json() 
+      settodo(data2);
+      setData(true);
+      setFetching(false);
+      console.log("Data = ",todo);
+      console.log("isData",isData);
+    }
+    fetchData();
+      }, [isData]
+  );
+
+   if(isFetching){
+    return <div>Loading ...</div>
+
+
+   }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+
+   Fetch data.
+  <span>Title: {todo.title}</span>
     </div>
   );
 }
